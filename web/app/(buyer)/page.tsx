5b3cfdaef9801a717a9efb4a50a8d00eb765b073
ENUID <accounts@enuid.com>
2026-05-09 09:03:12 +0700

    Fix build: pass initialBuyerContext to BuyerWorkspacePage

--- web/app/(buyer)/page.tsx
+++ web/app/(buyer)/page.tsx
@@ -1,1 +1,21 @@
-export { default } from '@/features/buyer/BuyerWorkspacePage'
+import { cookies, headers } from 'next/headers'
+import BuyerWorkspacePage from '@/features/buyer/BuyerWorkspacePage'
+import {
+  BUYER_COUNTRY_COOKIE,
+  BUYER_CURRENCY_COOKIE,
+  resolveBuyerContext,
+} from '@/lib/buyerContext'
+
+export default async function Page() {
+  const headerStore = await headers()
+  const cookieStore = await cookies()
+
+  const buyerContext = resolveBuyerContext({
+    countryHeader: headerStore.get('x-vercel-ip-country'),
+    acceptLanguage: headerStore.get('accept-language'),
+    cookieCountry: cookieStore.get(BUYER_COUNTRY_COOKIE)?.value,
+    cookieCurrency: cookieStore.get(BUYER_CURRENCY_COOKIE)?.value,
+  })
+
+  return <BuyerWorkspacePage initialBuyerContext={buyerContext} />
+}
